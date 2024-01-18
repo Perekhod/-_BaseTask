e1271e126e68bbc18415ec952211fa34f27ce3c9
Ilya <124806482+Perekhod@users.noreply.github.com>
2024-01-18 20:14:51 +0500

    Add files via upload

---
+++ AngryBirdsTask.cs
@@ -0,0 +1,24 @@
+﻿/*
+ В файле AngryBirdsTask реализуйте функцию расчета угла прицеливания,в зависимости от начальной
+скорости снаряда и дальности до цели. Если решения не существует, метод должен возвращать double.NaN
+ */
+
+using System;
+
+namespace AngryBirds
+{
+    public static class AngryBirdsTask
+    {
+        public static double FindSightAngle(double v, double distance)
+        {
+            const double g = 9.8; // ускорение свободного падения
+            double argument = (g * distance) / (v * v);
+
+            // Проверка на возможность достижения цели
+            if (argument > 1 || argument < 0)
+                return double.NaN;
+
+            return 0.5 * Math.Asin(argument);
+        }
+    }
+}
